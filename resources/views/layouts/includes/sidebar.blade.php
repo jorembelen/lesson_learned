09e4cf216e16ef7e9493a037e3d429204a01ad48
Jorem Belen <jorembelen@gmail.com>
2021-12-14 12:52:28 +0300

    new theme

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -12,6 +12,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/"><span class="shape1"></span><span class="shape2"></span><i class="ti-home sidemenu-icon"></i><span class="sidemenu-label">Home</span></a>
                 </li>
+                @if (auth()->user()->role == 5)
                 <li class="nav-item">
                     <a class="nav-link with-sub" href="#"><span class="shape1"></span><span class="shape2"></span><i class="ti-user sidemenu-icon"></i><span class="sidemenu-label">Users</span><i class="angle fe fe-chevron-right"></i></a>
                     <ul class="nav-sub">
@@ -20,6 +21,7 @@
                         </li>
                     </ul>
                 </li>
+                @endif
                 <li class="nav-item">
                     <a class="nav-link with-sub" href="#"><span class="shape1"></span><span class="shape2"></span><i class="ti-write sidemenu-icon"></i><span class="sidemenu-label">Lessons Learned</span><i class="angle fe fe-chevron-right"></i></a>
                     <ul class="nav-sub">
